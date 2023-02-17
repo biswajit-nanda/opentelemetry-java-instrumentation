77e590ae8c683e311adeb9d280841d6a5937491b
biswajit-nanda <biswajit.nanda@appdynamics.com>
2023-02-17 12:21:52 -0600

    Changes to support ECS ContianerID detection

--- instrumentation/resources/library/src/main/java/io/opentelemetry/instrumentation/resources/ContainerResource.java
+++ instrumentation/resources/library/src/main/java/io/opentelemetry/instrumentation/resources/ContainerResource.java
@@ -14,8 +14,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Optional;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import java.util.stream.Stream;
 
 /**
